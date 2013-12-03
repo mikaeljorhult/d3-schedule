77f9554f401427f07200b464196baa5d94efe041
Mikael Jorhult <mikael@jorhult.se>
2013-12-03 19:13:48 +0100

    Use RequireJS to load scripts.

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
-( function () {
+requirejs.config( {
+	paths: {
+		d3: 'd3.min'
+	},
+	shim: {
+		d3: {
+			exports: 'd3'
+		}
+	}
+} );
+
+define( [ 'd3', 'Events' ], function( d3, Events ) {
 	var container = d3.selectAll( '.schedule' ),
 		schedule = container.append( 'svg' ),
 		dateFormat = d3.time.format( '%Y-%m-%d %H:%M:%S' ),
 		timeScale,
-		objects,
-		width = 800,
+		objects = [],
+		width,
 		height = 400;
 	
 	// Set height and width of schedule.
-	schedule
-		.attr( 'width', width )
-		.attr( 'height', height );
+	d3.select( window ).on( 'resize', resize );
+	resize();
 	
 	// Get bookings.
 	d3.json( 'bookings.json', function( error, json ) {
 		objects = json;
 		
-		// Setup timescale.
-		timeScale = d3.time.scale()
-			.domain( [
-				d3.min( objects, function( datum ) {
-					return d3.min( datum.bookings, function( booking ) {
-						return dateFormat.parse( booking.start_time );
-					} );
-				} ),
-				d3.max( objects, function( datum ) {
-					return d3.max( datum.bookings, function( booking ) {
-						return dateFormat.parse( booking.end_time );
-					} );
-				} )
-			] )
-			.range( [ 0, width ] );
-		
-		// Render schedule.
-		visualize();
+		// Trigger event.
+		Events.publish( 'objects:updated' );
 	} );
 	
 	function visualize() {
+		if ( objects.length < 1 ) { return; }
+		
 		// Append objects.
 		schedule.selectAll( 'g.object' )
 			.data( objects )
@@ -61,4 +57,40 @@
 									.attr( 'fill', '#2d578b' );
 					} );
 	}
-} )();
+	
+	function getWidth() {
+		return parseInt( container.style( 'width' ), 10 );
+	}
+	
+	function resize() {
+		width = getWidth();
+		
+		schedule
+			.attr( 'width', width )
+			.attr( 'height', height );
+		
+		visualize();
+	}
+	
+	// Subscribe to events.
+	Events.subscribe( 'objects:updated', function () {
+		// Setup timescale.
+		timeScale = d3.time.scale()
+			.domain( [
+				d3.min( objects, function( datum ) {
+					return d3.min( datum.bookings, function( booking ) {
+						return dateFormat.parse( booking.start_time );
+					} );
+				} ),
+				d3.max( objects, function( datum ) {
+					return d3.max( datum.bookings, function( booking ) {
+						return dateFormat.parse( booking.end_time );
+					} );
+				} )
+			] )
+			.range( [ 0, width ] );
+		
+		// Render bookings.
+		visualize();
+	} );
+} );
